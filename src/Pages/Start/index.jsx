30501121e328b6c8698313c1697f4bc9797fef3b
Luan <tatalc4e@gmail.com>
2022-11-28 20:52:03 -0300

    criação da interface

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { View, ScrollView, Text, Image } from "react-native-gesture-handler";
+import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
 
 export default function Start() {
   return (
-    <View>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View>
-          <Image source={require("../../assets/icons/logo3.png")} />
-          <Text>
-            Vamos transformar sua vida {"/n"} em jogo, buscando sempre {"/n"} o
+    <View style={styles.container}>
+      <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={require("../../assets/icons/logo3.png")}
+            style={styles.logo}
+          />
+          <Text style={styles.description}>
+            Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"} o
             melhor nivel
           </Text>
         </View>
@@ -16,3 +19,22 @@ export default function Start() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21,21,21,0.98)",
+  },
+  logo: {
+    width: 300,
+    height: 60,
+    marginTop: 60,
+    marginBotton: 20,
+  },
+  description: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    textAlign: "center",
+    marginVertical: 60,
+  },
+});
