30c51fbcfe238f487481c172d7bf4c7e538617d6
Luan <tatalc4e@gmail.com>
2022-11-29 00:31:37 -0300

    status bar adicionadas e estilizadas

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,6 +3,7 @@ import { useNavigation } from "@react-navigation/native";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 
 import LifeStatus from "../../Components/Common/LifeStatus";
+import StatusBar from "../../Components/Home/StatusBar";
 
 export default function Home() {
   const navigation = useNavigation();
@@ -19,7 +20,7 @@ export default function Home() {
 
           <LifeStatus />
 
-          {/* <StatusBar /> */}
+          <StatusBar />
         </View>
 
         <Text
