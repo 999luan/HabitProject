37ced5b006f149dfba84042fa162abc4e1d7b84d
Luan <tatalc4e@gmail.com>
2022-12-07 01:36:14 -0300

    novas animacoes

--- src/Components/Common/LifeStatus/index.jsx
+++ src/Components/Common/LifeStatus/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+
 import { View, StyleSheet } from "react-native";
 import Lottie from "lottie-react-native";
 import AnimationService from "../../../Services/AnimationService";
@@ -36,11 +37,15 @@ export default function LifeStatus({
   }, [mindHabit, moneyHabit, bodyHabit, funHabit]);
 
   return (
-    <View style={styles.container}>
-      <Lottie source={mind} autoPlay loop style={styles.educacaoAnimacao} />
-      <Lottie source={money} autoPlay loop style={styles.financasAnimacao} />
-      <Lottie source={robot} autoPlay loop style={styles.roboAnimacao} />
-    </View>
+    <>
+      <View>
+        <Lottie source={robot} autoPlay loop style={styles.roboAnimacao} />
+      </View>
+      <View style={styles.container}>
+        <Lottie source={mind} autoPlay loop style={styles.educacaoAnimacao} />
+        <Lottie source={money} autoPlay loop style={styles.financasAnimacao} />
+      </View>
+    </>
   );
 }
 
@@ -50,10 +55,11 @@ const styles = StyleSheet.create({
     height: 300,
   },
   roboAnimacao: {
-    width: 190,
-    marginTop: 20,
-    marginLeft: 25,
-    zIndex: -1,
+    position: "absolute",
+    width: 390,
+    height: 300,
+    marginTop: 0,
+    marginLeft: -95,
   },
   educacaoAnimacao: {
     width: 100,
